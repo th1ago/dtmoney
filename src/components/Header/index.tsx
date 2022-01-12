7bbf5e647410e63ab6a580c3faf160f2a8af7f0c
Thiago <thiago_murakami@hotmail.com>
2022-01-12 15:08:42 -0300

    config modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 import logo from '../../assets/logo.svg'
+import Modal from 'react-modal';
 import { Container, Content } from './styles'
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logo} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova transacao
                 </button>
+
+                <Modal 
+                    isOpen={isNewTransactionModalOpen}
+                    onRequestClose={handleCloseNewTransactionModal}
+                >
+                    <h2>teste</h2>
+                </Modal>
             </Content>
         </Container>
     )
