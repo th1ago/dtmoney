18c280a414c5924431f69b63beb175434305b3c5
Thiago <thiago_murakami@hotmail.com>
2022-01-13 20:34:09 -0300

    css in the form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,8 +11,17 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <Modal 
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
+            <Container>
+                <h2>Cadastrar transacoes</h2>
 
+                <input placeholder='titulo'/>
+                <input type='number' placeholder='valor' />
+                <input placeholder='categoria' />
+                <button type="submit">Cadastrar</button>
+            </Container>
         </Modal>
     )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
 
     :root {
-        --background: #f0f2f5;
         --red: #E52E4D;
         --green: #33CC95;
         --blue: #5429CC;
@@ -54,6 +53,29 @@ export const GlobalStyle = createGlobalStyle`
     [disavled] {
         opacity: 0.6;
         cursor: not-allowed;
+    }
+
+    .react-modal-overlay{
+        background: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content{
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.24rem;
     }
 `
 
